b960678fd0187d5fd3c47bd5ec04deacfc1eb568
sniyaz <sniyaz@bk.ru>
2022-11-15 22:23:58 +0300

    Home Work Android 2.2 Fix PostViewModel

--- app/src/main/java/ru/netology/homeworkandroid13/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/homeworkandroid13/viewmodel/PostViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import ru.netology.homeworkandroid13.repository.PostRepository
 import ru.netology.homeworkandroid13.repository.PostRepositoryInMemoryImpl
 
-class PostViewModel {
+class PostViewModel: ViewModel() {
     private val repository: PostRepository = PostRepositoryInMemoryImpl()
     val data = repository.get()
     fun like() =repository.like()
